e7f8abacd4b6921932cfb036e78e9eb009565801
sukiasyan <shakob@gmail.com>
2025-04-05 09:13:08 +0200

    add client chache

--- app/components/general/Navbar.tsx
+++ app/components/general/Navbar.tsx
@@ -1,12 +1,13 @@
+'use client';
+
 import { buttonVariants} from '@/components/ui/button';
 import Link from 'next/link';
 import {RegisterLink, LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components"
-import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+import { useKindeBrowserClient} from "@kinde-oss/kinde-auth-nextjs";
 
-export async function Navbar() {
-  const {getUser} = getKindeServerSession();
-  const user = await getUser()
-
+export  function Navbar() {
+  const {getUser} = useKindeBrowserClient();
+  const user =  getUser()
 
   return (
     <nav className='py-5 flex items-center justify-between'>
